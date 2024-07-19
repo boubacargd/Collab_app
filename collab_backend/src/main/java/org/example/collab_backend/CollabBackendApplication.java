577cb7702983bf5493d96f971092d1ce1359ffa7
boubacargd <dev.boubacargadio@gmail.com>
2024-07-19 22:55:49 +0200

    install

---
+++ collab_backend/src/main/java/org/example/collab_backend/CollabBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.collab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabBackendApplication.class, args);
+    }
+
+}
